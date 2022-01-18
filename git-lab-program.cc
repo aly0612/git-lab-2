9e204d6d32c291d637991e9316b304ca1021fa57
Austin Ly <al044516@ohio.edu>
2022-01-18 12:31:54 -0500

    Lab02 code

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Austin Ly
+ *        Date: 01/18/2022
+ * Description: CS 2400 Lab 02
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
